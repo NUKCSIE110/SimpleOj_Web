8c32763626d847ab5fb130115c3ec93f63166574
ifTNT <happyjohn369@gmail.com>
2017-12-09 16:47:38 +0800

    Fixed jquery with turbolinks

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('ready turbolinks:load', function(){
   getTime();
   setInterval(getTime, 60000);
   setInterval(updateTime, 1000);
@@ -13,15 +13,15 @@ function getTime(){
 }
 function updateTime(){
   var t = new Date($("#timeLeft").data("exp") - Date.now());
-  //t = new Date(t.getTime() + $("#timeLeft").data("gap"));
+  t = new Date(t.getTime() + $("#timeLeft").data("gap"));
   if(t.getTime() >= 0){
     var strTimeLeft = ("0"+t.getUTCHours()).slice(-2) + ':';
     strTimeLeft += ("0"+t.getUTCMinutes()).slice(-2) + ':';
     strTimeLeft += ("0"+t.getUTCSeconds()).slice(-2);
-    //console.log(t.getTime());
+    console.log(t.getTime());
     $("#timeLeft").text(strTimeLeft);
   }else{
-    $("#timeLeft").text("00:00:00 TIME'S UP!");
+    $("#timeLeft").text("00:00:00");
     $("#timeLeft").addClass("text-danger");
   }
 }
